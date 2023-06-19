8067c54dc3f0c327f8ca397d7ee281b5291b0299
jdolata <juliadolata007@gmail.com>
2023-06-19 11:23:59 +0200

    frjfr

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ import Coliving from "./Components/Projects/Coliving";
 import RakosPatak from "./Components/Projects/RakosPatak";
 
 import "./App.css";
-import { Routes, Route, useLocation, BrowserRouter } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 
 function App() {
   const location = useLocation();
   return (
     <div className="App">
         <Routes location={location} key={location.key}>
-          <Route exact path="/" element={<Homepage />} />
+          <Route exact path="" element={<Homepage />} />
           <Route path="/childrenshouse" element={<ChildrensHouse />} />
           <Route path="/socialhousing" element={<SocialHousing />} />
           <Route path="/climbingcenter" element={<ClimbingCenter />} />
