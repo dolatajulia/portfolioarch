74d42ec0eb15f007d65fa07db3e9dcb319099d92
jdolata <juliadolata007@gmail.com>
2023-06-19 11:03:21 +0200

    dnsndn

--- src/App.js
+++ src/App.js
@@ -6,20 +6,22 @@ import Coliving from "./Components/Projects/Coliving";
 import RakosPatak from "./Components/Projects/RakosPatak";
 
 import "./App.css";
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation, BrowserRouter } from "react-router-dom";
 
 function App() {
   const location = useLocation();
   return (
-    <div className='App'>
-    <Routes location={location} key={location.key}>
-      <Route path="/" element={<Homepage />} />
-      <Route path="/childrenshouse" element={<ChildrensHouse />} />
-      <Route path="/socialhousing" element={<SocialHousing />} />
-      <Route path="/climbingcenter" element={<ClimbingCenter />} />
-      <Route path="/coliving" element={<Coliving />} />
-      <Route path="/rakospatak" element={<RakosPatak />} />
-    </Routes>
+    <div className="App">
+      <BrowserRouter basename={"/portfolioarch"}>
+        <Routes location={location} key={location.key}>
+          <Route exact path="/portfolioarch" element={<Homepage />} />
+          <Route path="/childrenshouse" element={<ChildrensHouse />} />
+          <Route path="/socialhousing" element={<SocialHousing />} />
+          <Route path="/climbingcenter" element={<ClimbingCenter />} />
+          <Route path="/coliving" element={<Coliving />} />
+          <Route path="/rakospatak" element={<RakosPatak />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
